34e2c59c4aa88e0159cc45183dbbebcb00394d19
lysianel <lysiane.l@hotmail.fr>
2020-10-03 14:22:01 +0200

    Updates

---
+++ precache-manifest.fff6d095ee1ea92d69816f7fc028b66c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84e401d39085de29d8d99f3b7aafa0e2",
+    "url": "/FCC3-RandomQuoteMachine/index.html"
+  },
+  {
+    "revision": "d75acb3944473be4470f",
+    "url": "/FCC3-RandomQuoteMachine/static/css/main.450928df.chunk.css"
+  },
+  {
+    "revision": "db86ffb2a4a1cda39dbb",
+    "url": "/FCC3-RandomQuoteMachine/static/js/2.72db6294.chunk.js"
+  },
+  {
+    "revision": "bfd04eb851fbc8ea43c375cc9d698376",
+    "url": "/FCC3-RandomQuoteMachine/static/js/2.72db6294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d75acb3944473be4470f",
+    "url": "/FCC3-RandomQuoteMachine/static/js/main.e9d5f569.chunk.js"
+  },
+  {
+    "revision": "4be31401a8ee461ca26f",
+    "url": "/FCC3-RandomQuoteMachine/static/js/runtime-main.6ee40de6.js"
+  }
+]);
